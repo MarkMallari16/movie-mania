badf97271c81a64af76d125b3334a771e2ca0e53
MarkMallari16 <mallariftw01@gmail.com>
2024-07-28 13:26:02 +0800

    fix some ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 
 
-import { Navbar } from 'react-bootstrap';
+
 import './App.css'
+import Navbar from './components/Navbar';
 import useFetch from './hooks/useFetch'
 
 function App() {
   const { data, loading } = useFetch("https://api.themoviedb.org/3/movie/popular?api_key=24ce4eec248652f741c228a1d8a1a21c");
 
   if (loading) return <p>Loading....</p>
-
+ console.log(data)
   return (
-    <div className='bg-slate-900'>
+    <div className='min-h-screen bg-slate-900'>
       <Navbar />
-      {data && data.results.map((d) => (
-        <div key={d.id} className='flex'>
+
+      <div className='mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+        {data && data.results.map((d, _) => (
           <div >
             <img src={`https://image.tmdb.org/t/p/w200${d.poster_path}`} alt={d.title} />
-            <h2>{d.title}</h2>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
-            <a className="btn btn-ghost text-xl">MovieMania</a>
+        <div className="navbar bg-base-content">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-2xl text-blue-500">MovieMania</a>
+            </div>
         </div>
     )
 }
