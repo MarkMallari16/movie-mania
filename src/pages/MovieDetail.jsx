c62a4196b2126ddddd8a029f4a6e51c01401b3dd
MarkMallari16 <mallariftw01@gmail.com>
2024-07-28 16:04:25 +0800

    fix some ui

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,9 +4,11 @@ import useFetch from '../hooks/useFetch';
 
 const MovieDetail = () => {
     const { id } = useParams();
-    const { data: movieDetail, loading } = useFetch(`https://api.themoviedb.org/3/movie/${id}?api_key=24ce4eec248652f741c228a1d8a1a21c`)
+    const { data: movieDetail, loading: movieLoading } = useFetch(`https://api.themoviedb.org/3/movie/${id}?api_key=24ce4eec248652f741c228a1d8a1a21c`)
+    const { data: videoData, loading: videoLoading } = useFetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=24ce4eec248652f741c228a1d8a1a21c`);
 
-    if (loading) return <p>Loading...</p>;
+    console.log(videoData)
+    if (movieLoading) return <p>Loading...</p>;
 
     const backdropUrl = `https://image.tmdb.org/t/p/original${movieDetail.backdrop_path}`;
     console.log(movieDetail)
@@ -16,30 +18,28 @@ const MovieDetail = () => {
                 <div>
                     <h1 className='text-5xl lg:text-7xl font-bold'>{movieDetail.title}</h1>
                     <div className='items-center flex gap-4 mt-4'>
-                        <p className='flex items-center gap-1'>
+                        <div className='flex items-center gap-1'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
                             </svg>
 
                             <p>{movieDetail.vote_average}</p>
-                        </p>
-                        <p className='flex items-center gap-1'>
+                        </div>
+                        <div className='flex items-center gap-1'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
                             </svg>
 
 
                             <p>{movieDetail.release_date}</p>
-                        </p>
-                        <p className='flex items-center gap-1 '>
+                        </div>
+                        <div className='flex items-center gap-1 '>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
                             </svg>
 
-
-
                             <p>{movieDetail.runtime}</p>
-                        </p>
+                        </div>
                     </div>
                     <p className='mt-4 lg:w-1/2 '>{movieDetail.overview}</p>
 
