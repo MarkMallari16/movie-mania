6102639d1d906391bbbdaee94b2e5e0ced5c6073
MarkMallari16 <mallariftw01@gmail.com>
2024-07-31 02:20:03 +0800

    add search

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -41,7 +41,7 @@ const MovieDetail = () => {
                     width="100%"
                     height="100vh"
 
-                    onEnded={() => setIsTrailerEnded(true)}
+                    onEnded={() => setIsTeaserEnded(true)}
                     className='absolute top-0 left-0 w-full h-full z-0 bg-cover' />
             )}
 
@@ -57,7 +57,7 @@ const MovieDetail = () => {
                     <h1 className='text-5xl lg:text-7xl font-bold'>{movieDetail.title}</h1>
                     <div className='items-center flex gap-4 mt-4'>
                         <div className='flex items-center gap-1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-blue-500 size-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-yellow-500 size-6">
                                 <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
                             </svg>
                             <p>{movieDetail.vote_average}</p>
