4b5745d538b2360acf4b2de7eca9c145be868238
MarkMallari16 <mallariftw01@gmail.com>
2024-07-30 11:07:46 +0800

    convert movie carousel into array of objects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
-import MovieCard from '../components/MovieCard'
+import PropTypes from 'prop-types'
 import Navbar from '../components/Navbar'
 import MovieCarousel from '../components/MovieCarousel'
 
 const Home = ({ nowPlaying, popular, topRated, upcoming }) => {
+    const movieCarousels = [
+        {
+            title: "Now Playing",
+            data: nowPlaying,
+        },
+        {
+            title: "Popular",
+            data: popular,
+        },
+        {
+            title: "Top Rated",
+            data: topRated,
+        },
+        {
+            title: "Upcoming",
+            data: upcoming,
+        }
+
+    ]
     return (
         <>
             <Navbar />
             <div className='mx-10'>
-                <MovieCarousel carouselTitle="Now Playing" movies={nowPlaying} />
-                <MovieCarousel carouselTitle="Popular" movies={popular} />
-                <MovieCarousel carouselTitle="Now Playing" movies={topRated} />
-                <MovieCarousel carouselTitle="Upcoming" movies={upcoming} />
+                {movieCarousels.map((movie, index) => (
+                    <MovieCarousel key={index} carouselTitle={movie.title} movies={movie.data} />
+                ))}
             </div>
         </>
     )
+}
+
+Home.propTypes = {
+    nowPlaying: PropTypes.array.isRequired,
+    popular: PropTypes.array.isRequired,
+    topRated: PropTypes.array.isRequired,
+    upcoming: PropTypes.array.isRequired,
 }
 
 export default Home
