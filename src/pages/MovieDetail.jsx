5eb00b0ca471daf3bbd3a97d8187f08d446c807c
MarkMallari16 <mallariftw01@gmail.com>
2024-07-30 18:47:43 +0800

    change into teaser

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import ReactPlayer from 'react-player';
 
@@ -9,31 +9,49 @@ const MovieDetail = () => {
     const { data: videoData, loading: videoLoading } = useFetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=24ce4eec248652f741c228a1d8a1a21c`);
 
     const [isTrailerEnded, setIsTrailerEnded] = useState(false);
+    const [isHeartFill, setIsHeartFill] = useState(false);
+    const [isDelayed, setIsDelayed] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsDelayed(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [])
 
     if (movieLoading || videoLoading) return <p>Loading...</p>;
 
-    const trailer = videoData?.results?.find(video => video.type === "Trailer" && video.site === "YouTube");
+    const trailer = videoData?.results?.find(video => video.type === "Teaser" && video.site === "YouTube");
     const trailerUrl = trailer ? `https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=1&loop=1&playlist=${trailer.key}&controls=0&showinfo=0&modestbranding=1&rel=0` : null;
 
     const backdropUrl = `https://image.tmdb.org/t/p/original${movieDetail.backdrop_path}`;
 
     return (
-        <div className={`min-h-screen w-full text-white relative ${isTrailerEnded && opacity-100} transition-all ease-in-out`} style={{
-            backgroundImage: `${isTrailerEnded} ? url(${backdropUrl}): none`,
+        <div className={`min-h-screen w-full  text-white relative  transition-all ease-in-out`} style={{
+            backgroundImage: trailerUrl === null || isDelayed || isTrailerEnded ? `url(${backdropUrl})` : 'none',
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         }}>
-            {!isTrailerEnded && trailerUrl && (
+            {!isDelayed && (
                 <ReactPlayer
                     url={trailerUrl}
                     playing
                     muted
                     width="100%"
                     height="100vh"
+
                     onEnded={() => setIsTrailerEnded(true)}
                     className='absolute top-0 left-0 w-full h-full z-0 bg-cover' />
             )}
 
+            <div className='absolute z-20 left-10 top-6'>
+                <Link to='/' className='btn'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 w-full">
+                        <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+                    </svg>
+                </Link>
+            </div>
             <div className='bg-slate-900 bg-opacity-75 min-h-screen flex items-center p-10 lg:p-20 relative z-10'>
                 <div>
                     <h1 className='text-5xl lg:text-7xl font-bold'>{movieDetail.title}</h1>
@@ -65,10 +83,14 @@ const MovieDetail = () => {
                             </svg>
                             <span>Watch Trailer</span>
                         </button>
-                        <button className='btn btn-ghost'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                            </svg>
+                        <button className='btn btn-ghost' onClick={() => setIsHeartFill(!isHeartFill)}>
+                            {isHeartFill ?
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                                </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                            }
                             <span>Add to list</span>
                         </button>
                     </div>
