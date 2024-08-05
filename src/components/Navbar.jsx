0b687dbbf64fdba15b7531597ba9a7d4814ef017
MarkMallari16 <mallariftw01@gmail.com>
2024-08-05 18:43:47 +0800

    fix some ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,47 +2,33 @@ import React, { useRef, useState } from 'react'
 import Logo from '../assets/logo1.png'
 import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import Profile from '../assets/profile.jpg'
 const Navbar = () => {
-    const modalRef = useRef(null);
-    const [query, setQuery] = useState("");
 
-
-    const [searchUrl, setSearchUrl] = useState("");
-    const { data, loading } = useFetch(searchUrl);
-
-    const handleOpenSearchModal = () => {
-        if (modalRef.current) {
-            modalRef.current.showModal();
-        }
-    }
-    const handleCloseSearchModal = () => {
-        if (modalRef.current) {
-            modalRef.current.close();
-        }
-    }
-
-    const handleSearchInputOpen = () => {
-        setIsSearchInputOpen(!isSearchInputOpen);
-    }
-    const handleSearch = () => {
-        const url = `https://api.themoviedb.org/3/search/movie?query=${query}`
-        setSearchUrl(url);
-    }
     return (
         <div className="navbar pt-4" >
             <div className='flex justify-between w-full mx-5'>
                 <div>
-                    <div>
-                        <img src={Logo} alt="MovieMania" className='w-full' />
-                    </div>
+                    <Link to='/'>
+                        <div>
+                            <img src={Logo} alt="MovieMania" className='w-full' />
+                        </div>
+                    </Link>
                 </div>
 
-                <div className='flex items-center'>
+                <div className='relative   w-full max-w-2xl'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="inset-3 size-6 absolute">
+                        <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+                    </svg>
+                    <input type="text" className='input input-bordered w-full pl-12' placeholder='Search movies here...' />
+                </div>
+
+                <div className='flex items-center gap-5'>
                     <ul className="menu menu-horizontal px-1 text-white uppercase font-medium">
                         <li>
                             <Link>Movies</Link>
                         </li>
-                       
+
                         <li>
                             <Link to='/movies/popular'>Popular</Link>
                         </li>
@@ -50,39 +36,26 @@ const Navbar = () => {
                             <Link>Top Rated</Link>
                         </li>
                     </ul>
-                </div>
 
-                <div className='flex items-center gap-8'>
-                    <div className='text-white'>|</div>
-                    <div className='text-white btn btn-ghost' onClick={handleOpenSearchModal}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
-            <dialog id="my_modal_2" className="modal" ref={modalRef}>
-                <div className="modal-box bg-slate-900">
+                    <div className="dropdown dropdown-end">
 
-
-                    <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className='mt-6 input input-bordered w-full' placeholder='Search here...' />
-                    <div className='flex gap-3'>
-                        <button className='mt-4 btn btn-secondary' onClick={handleSearch}>Search</button>
-                        <button className='mt-4 btn ' onClick={handleCloseSearchModal}>Close</button>
-                    </div>
-
-                    {loading && <p>Loading...</p>}
-                    {data && data.results && (
-                        <div>
-                            {data.results.map((movie) => (
-                                <div key={movie.id} className='text-white'>
-                                    {movie.title}
-                                </div>
-                            ))}
+                        <div tabIndex={0} role="button" className="avatar online">
+                            <div className="w-12 rounded-full object-cover">
+                                <img src={Profile} />
+                            </div>
                         </div>
-                    )}
+
+
+                        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a href="/profile">View Profile</a></li>
+                            <li><a href="/settings">Settings</a></li>
+                            <li><a href="/logout">Logout</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </dialog>
+
+
+            </div>
         </div>
     )
 }
