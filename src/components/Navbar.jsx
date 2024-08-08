8a99ec2f184bfe352e5d10658a79f34631396168
MarkMallari16 <mallariftw01@gmail.com>
2024-08-09 00:18:34 +0800

    add profile and home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,15 @@ import Profile from '../assets/profile.jpg'
 export const NAV_LINKS = [
     {
         icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+        </svg>
+        ,
+        title: "Home",
+        path: "/"
+    },
+    {
+        icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
             <path fillRule="evenodd" d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z" clipRule="evenodd" />
         </svg>,
         title: "Popular",
@@ -94,7 +103,7 @@ const Navbar = () => {
 
                         <div tabIndex={0} role="button" className="avatar online">
                             <div className="w-12 rounded-full object-cover">
-                                <img src={Profile} alt='Profile'/>
+                                <img src={Profile} alt='Profile' />
                             </div>
                         </div>
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import ProfilePicture from '../assets/profile.jpg'
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='mx-20 mt-10 bg-base-200 p-10 rounded-lg'>
+      <div className='flex  items-center gap-5'>
+        <img src={ProfilePicture} alt="profile" className='w-52 rounded-full' />
+        <div>
+          <h1 className='font-bold text-2xl'>Mark Christian Mallari</h1>
+          <p className='mt-2 max-w-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At iusto numquam itaque rem, doloribus repudiandae quisquam fugit placeat quibusdam accusantium?</p>
+        </div>
+      </div>
+      <div className='mt-10'>
+        <h2 className='font-semibold text-xl'>Favorite Genres</h2>
+        <ul className='list-disc list-inside'>
+          <li>Thriller</li>
+          <li>Science Fiction</li>
+          <li>Action</li>
+        </ul>
+      </div>
+      <div className='mt-10'>
+        <h2 className='font-semibold text-xl'>Recent Activity</h2>
+        <ul className='list-disc list-inside'>
+          <li>Watched "Inception" - Rated 5 stars</li>
+          <li>Reviewed "The Matrix"</li>
+          <li>Added "Blade Runner 2049" to Watchlist</li>
+        </ul>
+      </div>
+      <div className='mt-10'>
+        <h2 className='font-semibold text-xl'>Profile Settings</h2>
+        <button className='btn btn-secondary mt-2'>Edit Profile</button>
+      </div>
+    </div>
   )
 }
 
