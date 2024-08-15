745e226f9f65dd344e4e9de26bc036113a0eea10
MarkMallari16 <mallariftw01@gmail.com>
2024-08-15 22:35:37 +0800

    refactor: changing the  ui of person page

--- src/pages/PersonPage.jsx
+++ src/pages/PersonPage.jsx
@@ -3,16 +3,32 @@ import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
 
 const PersonPage = () => {
-  const { id } = useParams();
+  const { id, name } = useParams();
 
   const { data: personData, loading: personLoading } = useFetch(`https://api.themoviedb.org/3/person/${id}`);
 
-  if (personLoading){
+  if (personLoading) {
     return <div>Loading...</div>
   }
+
+  console.log(personData)
   return (
     <div className='lg:mt-28 mx-12'>
-      {personData.name}
+      <div className='flex flex-wrap lg:flex-nowrap gap-8 '>
+
+        <img src={`https://image.tmdb.org/t/p/w300${personData.profile_path}`} alt={personData.name} className='rounded-lg w-full lg:w-auto' />
+        <div>
+          <h1 className='text-6xl font-semibold'>{personData.name}</h1>
+          <div>
+            <h2 className='text-xl font-medium mt-4'>Biography</h2>
+            <p className='mt-2 tracking-wide'>{personData.biography}</p>
+          </div>
+        </div>
+
+
+
+      </div>
+
     </div>
   )
 }
