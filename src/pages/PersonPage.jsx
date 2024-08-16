80d4a2ee81aaaa7a41c93c0670009b72b992e154
MarkMallari16 <mallariftw01@gmail.com>
2024-08-16 19:33:36 +0800

    add features of show more and show less

--- src/pages/PersonPage.jsx
+++ src/pages/PersonPage.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
 
 const PersonPage = () => {
-  const { id, name } = useParams();
+  const [isShowFullText, setIsShowFullText] = useState(false);
+
+  const { id } = useParams();
 
   const { data: personData, loading: personLoading } = useFetch(`https://api.themoviedb.org/3/person/${id}`);
 
   if (personLoading) {
     return <div>Loading...</div>
   }
+  const truncatedBiographyText = personData.biography.slice(0, 300);
+  const shouldShowFullText = personData.biography.length > truncatedBiographyText.length;
 
-  console.log(personData)
+  const handleToggleText = () => {
+    setIsShowFullText(!isShowFullText);
+  }
   return (
     <div className='lg:mt-28 mx-12'>
       <div className='flex flex-wrap lg:flex-nowrap gap-8 '>
 
-        <img src={`https://image.tmdb.org/t/p/w300${personData.profile_path}`} alt={personData.name} className='rounded-lg w-full lg:w-auto' />
+        <img src={`https://image.tmdb.org/t/p/w500${personData.profile_path}`} alt={personData.name} className='rounded-lg w-full lg:w-auto lg:h-96' />
         <div>
           <h1 className='text-6xl font-semibold'>{personData.name}</h1>
           <div>
             <h2 className='text-xl font-medium mt-4'>Biography</h2>
-            <p className='mt-2 tracking-wide'>{personData.biography}</p>
+            <p className='mt-2 tracking-wide '>
+              {isShowFullText ? personData.biography : truncatedBiographyText}
+            </p>
+            {shouldShowFullText && (
+              <span
+                onClick={handleToggleText}
+                className=' cursor-pointer text-secondary font-medium'
+              >
+                {isShowFullText ? 'See Less' : 'See More'}
+              </span>
+            )}
           </div>
           <div>
             <h2 className='text-xl font-medium mt-4'>Known For</h2>
