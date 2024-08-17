ce6dcbdbd136217171103fd20bf4bf8b0e712d25
MarkMallari16 <mallariftw01@gmail.com>
2024-08-18 07:55:35 +0800

    add formatted age utils

--- src/utils/dateFormatUtils.js
+++ src/utils/dateFormatUtils.js
@@ -1,5 +1,6 @@
 export const formattedDate = (birthDate) => {
     const date = new Date(birthDate);
+
     const monthNames = [
         "January",
         "February",
@@ -18,5 +19,13 @@ export const formattedDate = (birthDate) => {
     const day = date.getDate();
     const year = date.getFullYear();
 
+
     return `${month} ${day}, ${year}`;
+}
+export const formattedAge = (birthDate) => {
+    const birthYear = new Date(birthDate).getFullYear();
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+
+    return `(${age} years old)`;
 }
