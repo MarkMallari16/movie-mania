949fbedfa02e713878f8c39832bd358feedc9fda
MarkMallari16 <mallariftw01@gmail.com>
2024-08-19 07:11:17 +0800

    fix drop down minor structure

--- src/components/SearchInputDropDown.jsx
+++ src/components/SearchInputDropDown.jsx
@@ -4,7 +4,7 @@ const SearchInputDropDown = ({ currentQuery, currentSearchResults, onHandleMovie
     return (
         <>
             {currentQuery && (
-                <div className='absolute mt-2 w-full max-h-60 bg-slate-900 overflow-y-auto  rounded-xl'>
+                <div className='absolute mt-2 w-full max-h-60 bg-slate-900 overflow-y-auto rounded-xl'>
                     {currentSearchResults.length > 0 ? (
                         currentSearchResults.map(movie => (
                             <div
@@ -12,7 +12,10 @@ const SearchInputDropDown = ({ currentQuery, currentSearchResults, onHandleMovie
                                 className='flex gap-5 p-4 hover:bg-slate-700 transition-colors ease-out cursor-pointer rounded-xl'
                                 onClick={() => onHandleMovieClick(movie)}
                             >
-                                {!movie.poster_path ? <div className='w-25'>Poster not available</div> : <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} className='max-w-12 rounded-xl' />}
+                                {!movie.poster_path ?
+                                    <div className='w-25'>Poster not available</div>
+                                    :
+                                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} className='max-w-12 rounded-xl' />}
                                 <div>
                                     <p>{movie.title}</p>
                                     <span className='text-slate-400'>{new Date(movie.release_date).getFullYear()}</span>
