f4c5836865e80e52306971f34196b39cdf2c5d10
MarkMallari16 <mallariftw01@gmail.com>
2024-08-24 05:44:01 +0800

    add lazy loading on home card

--- src/components/CarouselContainer.jsx
+++ src/components/CarouselContainer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import MovieSmallCard from './MovieSmallCard'
+import React, { lazy, Suspense } from 'react'
+import LazyLoadingCard from './LazyLoadingCard'
+
+const MovieSmallCard = lazy(() => import('./MovieSmallCard'))
 
 const CarouselContainer = ({ movieData }) => {
     return (
         <div className="flex carousel rounded-box w-full">
             <div className="carousel-item flex gap-6">
-                {movieData &&  movieData.results.slice(0, 20).map((data, _) => (
-                    <MovieSmallCard key={data.id} id={data.id} poster={data.poster_path} rate={data.vote_average} releaseDate={new Date(data.release_date).getFullYear()} title={data.title} />
+                {movieData && movieData.results.slice(0, 20).map((data, _) => (
+                    <Suspense fallback={<LazyLoadingCard />}>
+                        <MovieSmallCard key={data.id} id={data.id} poster={data.poster_path} rate={data.vote_average} releaseDate={new Date(data.release_date).getFullYear()} title={data.title} />
+                    </Suspense>
                 ))}
             </div>
         </div>
