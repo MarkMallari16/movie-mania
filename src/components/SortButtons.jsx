71686324e64e16e8b43ac475a26c76b8c5f82f98
MarkMallari16 <mallariftw01@gmail.com>
2024-08-24 05:20:47 +0800

    refactor structure of classNames in Sort Button component

--- src/components/SortButtons.jsx
+++ src/components/SortButtons.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const SortButtons = ({ onSorted, currentSort }) => {
+    const buttonClasses = `btn rounded-3xl btn-outline font-medium`
+
+    const getClassNames = sortType => {
+        return `${buttonClasses} ${currentSort === sortType ? 'btn-active btn-secondary' : ''}`;
+    }
+    
     return (
         <ul className='flex gap-5'>
-            <li onClick={() => onSorted('newest')} className={`btn rounded-3xl btn-outline  font-normal ${currentSort === 'newest' && 'btn btn-secondary'}`}>Newest</li>
-            <li onClick={() => onSorted('oldest')} className={`btn  rounded-3xl btn-outline font-normal ${currentSort === 'oldest' && 'btn-secondary'}`}>Oldest</li>
-            <li onClick={() => onSorted('highest')} className={`btn rounded-3xl btn-outline font-normal ${currentSort === 'highest' && 'btn-secondary'}`}>Highest</li>
-            <li onClick={() => onSorted('lowest')} className={`btn  rounded-3xl btn-outline font-normal ${currentSort === 'lowest' && 'btn-secondary'}`}>Lowest</li>
+            <li onClick={() => onSorted('newest')} className={getClassNames('newest')}>Newest</li>
+            <li onClick={() => onSorted('oldest')} className={getClassNames('oldest')}>Oldest</li>
+            <li onClick={() => onSorted('highest')} className={getClassNames('highest')}>Highest</li>
+            <li onClick={() => onSorted('lowest')} className={getClassNames('lowest')}>Lowest</li>
         </ul>
     )
 }
