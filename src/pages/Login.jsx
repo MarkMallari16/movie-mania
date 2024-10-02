a17b4eef22f269877eab610561c22ded7c088915
MarkMallari16 <mallariftw01@gmail.com>
2024-10-02 23:50:34 +0800

    add sample log in option like google and facebook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 const Login = () => {
     return (
         <div className='min-h-screen grid place-items-center'>
-            <div className='bg-base-300 p-10 rounded-lg w-1/3'>
-                <h1 className='text-2xl font-bold mb-4'>Movie<span className='text-blue-500'>Mania</span></h1>
+            <div className='bg-base-300 p-10 rounded-lg lg:w-1/3'>
+                <h1 className='text-3xl font-bold mb-4'>Movie<span className='text-blue-500'>Mania</span></h1>
                 <div>
                     <label htmlFor="email">Email</label>
                     <input type="text" className='block input input-bordered w-full' />
                 </div>
-                <div>
+                <div className='mt-4'>
                     <label htmlFor="email">Password</label>
                     <input type="text" className='block input input-bordered w-full' />
                 </div>
                 <div className='mt-4'>
-                    <button className='btn bg-blue-500 w-full'>Login</button>
+                    <button className='btn bg-blue-500 w-full font-bold'>Login</button>
                 </div>
-
+                <div className='mt-4 '>
+                    <button className='w-full flex justify-center items-center gap-2 bg-base-100 p-3 rounded-lg'>
+                        <FcGoogle className='text-xl'/>
+                        Continue with Google
+                    </button>
+                    <button className='mt-2 w-full flex justify-center items-center gap-2 bg-base-100 p-3 rounded-lg'>
+                        <FaFacebook className='text-xl'/>
+                        Continue with Facebook
+                    </button>
+                </div>
             </div>
         </div>
     )
